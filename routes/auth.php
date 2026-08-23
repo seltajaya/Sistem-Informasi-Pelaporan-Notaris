fa3542d34ebbb7e4791511575a91389b9bb07fd7
seltajaya <88418736+seltajaya@users.noreply.github.com>
2026-08-23 20:28:17 +0700

    perf: hapus route name login duplikat (blokir route:cache)

--- routes/auth.php
+++ routes/auth.php
@@ -11,10 +11,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    // GET/POST /login sudah ditangani /login/{slug?} di routes/web.php (name: login)
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
